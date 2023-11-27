22d6ac8af5f387aff47ab13e7873bf2db3ef966e
Rick Drumond <18102985+rickdrumond@users.noreply.github.com>
2023-11-27 00:21:19 -0300

    add smooth scrolling to nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,25 @@
-window.toggleMenu = button => {
-  button.classList.toggle('tham-active');
-  document.getElementById('menu').classList.toggle('is-active');
-}
+window.toggleMenu = (button) => {
+  button.classList.toggle("tham-active");
+  document.getElementById("menu").classList.toggle("is-active");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("menu").addEventListener(
+    "click",
+    (e) => {
+      if (e.target.tagName === "A") {
+        e.preventDefault();
+
+        let menuToggler = document.getElementById("menu-toggler");
+        if (menuToggler.classList.contains("tham-active")) {
+          toggleMenu(menuToggler);
+        }
+
+        document.querySelector(e.target.hash).scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    },
+    true
+  );
+});
