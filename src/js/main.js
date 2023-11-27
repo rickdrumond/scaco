e538f88f48a9a45b7d99bb93ea36d7eb77b26c31
Rick Drumond <18102985+rickdrumond@users.noreply.github.com>
2023-11-27 03:06:14 -0300

    add smooth scrolling to all anchors

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,39 @@
-window.toggleMenu = (button) => {
-  button.classList.toggle("tham-active");
-  document.getElementById("menu").classList.toggle("is-active");
+// Check if an 'a' element links to the same page
+const isAnchor = (element) => {
+  return (
+    element.hash &&
+    window.location.origin === element.origin &&
+    window.location.pathname === element.pathname
+  );
 };
 
+// Enable smooth scrolling for same-page links
+document.addEventListener("click", (e) => {
+  if ((a = e.target.closest("a")) && isAnchor(a)) {
+    e.preventDefault();
+
+    document.querySelector(a.hash).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("menu").addEventListener(
+  const menuToggler = document.getElementById("menu-toggler");
+  const menu = document.getElementById("menu");
+
+  const toggleMenu = () => {
+    menuToggler.classList.toggle("tham-active");
+    menu.classList.toggle("is-active");
+  };
+
+  menuToggler.addEventListener("click", toggleMenu);
+
+  // Close menu after clicking a link
+  menu.addEventListener(
     "click",
     (e) => {
-      if (e.target.tagName === "A") {
-        e.preventDefault();
-
-        let menuToggler = document.getElementById("menu-toggler");
-        if (menuToggler.classList.contains("tham-active")) {
-          toggleMenu(menuToggler);
-        }
-
-        document.querySelector(e.target.hash).scrollIntoView({
-          behavior: "smooth",
-        });
-      }
+      if (e.target.tagName === "A") toggleMenu();
     },
     true
   );
