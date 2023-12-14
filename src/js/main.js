ff9a6678d9b39e40649df9d5a0e795d73c77a952
Rick Drumond <18102985+rickdrumond@users.noreply.github.com>
2023-12-14 15:07:18 -0300

    comment-out everything related to the contact form

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,62 @@
 // Check if an 'a' element links to the same page
-const isAnchor = (element) => {
-  return (
-    element.hash &&
-    window.location.origin === element.origin &&
-    window.location.pathname === element.pathname
-  );
-};
+const isAnchor = (element) => (
+  element.hash &&
+  window.location.origin === element.origin &&
+  window.location.pathname === element.pathname
+);
 
-const submitContactForm = (recaptchaToken) => {
-  const form = document.querySelector("#contact-form");
-  const fd = new FormData(form);
-  const payload = {
-    person: {
-      firstName: fd.get("fname"),
-      lastName: fd.get("lname"),
-    },
-    email: fd.get("email"),
-    phone: fd.get("phone"),
-    city: fd.get("city"),
-    hearAbout: fd.get("hearAbout"),
-    areYouRealtor: fd.get("areYouRealtor"),
-    contactMethod: fd.getAll("contact-method"),
-    agreeToConsent: fd.get("agreeToConsent"),
-    agreeToTerms: fd.get("agreeToTerms"),
-    ReCAPTCHA: recaptchaToken,
-  };
+// const submitContactForm = (recaptchaToken) => {
+//   const form = document.querySelector("#contact-form");
+//   const fd = new FormData(form);
+//   const payload = {
+//     person: {
+//       firstName: fd.get("fname"),
+//       lastName: fd.get("lname"),
+//     },
+//     email: fd.get("email"),
+//     phone: fd.get("phone"),
+//     city: fd.get("city"),
+//     hearAbout: fd.get("hearAbout"),
+//     areYouRealtor: fd.get("areYouRealtor"),
+//     contactMethod: fd.getAll("contact-method"),
+//     agreeToConsent: fd.get("agreeToConsent"),
+//     agreeToTerms: fd.get("agreeToTerms"),
+//     ReCAPTCHA: recaptchaToken,
+//   };
 
-  debugger; // FIXME: remove
+//   debugger; // FIXME: remove
 
-  fetch(form.action, {
-    method: "POST",
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => {
-      console.log("Resposta do servidor:", response);
-      if (!response.ok) {
-        // TODO: user feedback
-        throw new Error("Erro ao enviar o formulário");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Resposta do servidor:", data);
-      // TODO: user feedback
-      alert("Sent!");
-      // exibirModalSucesso();
-    })
-    .catch((error) => {
-      // TODO: user feedback
-      console.error("Erro:", error);
-    });
-};
+//   fetch(form.action, {
+//     method: "POST",
+//     mode: "no-cors",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(payload),
+//   })
+//     .then((response) => {
+//       console.log("Resposta do servidor:", response);
+//       if (!response.ok) {
+//         // TODO: user feedback
+//         throw new Error("Erro ao enviar o formulário");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log("Resposta do servidor:", data);
+//       // TODO: user feedback
+//       alert("Sent!");
+//       // exibirModalSucesso();
+//     })
+//     .catch((error) => {
+//       // TODO: user feedback
+//       console.error("Erro:", error);
+//     });
+// };
 
-window.onRecaptchaSuccess = function (token) {
-  submitContactForm(token);
-};
+// window.onRecaptchaSuccess = function (token) {
+//   submitContactForm(token);
+// };
 
 // Enable smooth scrolling for same-page links
 document.addEventListener("click", (e) => {
@@ -88,7 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const menuToggler = document.getElementById("menu-toggler");
   const menu = document.getElementById("menu");
-  const contactForm = document.getElementById("contact-form");
+  // const contactForm = document.getElementById("contact-form");
 
   const toggleMenu = () => {
     menuToggler.classList.toggle("tham-active");
@@ -106,11 +104,11 @@ document.addEventListener("DOMContentLoaded", () => {
     true
   );
 
-  contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // Form will be submitted by ReCAPTCHA's callback (fn onRecaptchaSuccess)
-    grecaptcha.execute();
-  });
+  // contactForm.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   // Form will be submitted by ReCAPTCHA's callback (fn onRecaptchaSuccess)
+  //   grecaptcha.execute();
+  // });
 
   document
     .getElementById("privacy-policy-link")
@@ -125,29 +123,29 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
-  // Show/Hide ReCAPTCHA's badge if form is in viewport
-  const handleIntersection = (entries, _observer) => {
-    const recaptchaBadge = document.querySelector(".grecaptcha-badge");
-    if (!recaptchaBadge) return;
+  // // Show/Hide ReCAPTCHA's badge if form is in viewport
+  // const handleIntersection = (entries, _observer) => {
+  //   const recaptchaBadge = document.querySelector(".grecaptcha-badge");
+  //   if (!recaptchaBadge) return;
 
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // Form is visible, display badge
-        recaptchaBadge.style.visibility = "visible";
-      } else {
-        recaptchaBadge.style.visibility = "hidden";
-      }
-    });
-  };
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       // Form is visible, display badge
+  //       recaptchaBadge.style.visibility = "visible";
+  //     } else {
+  //       recaptchaBadge.style.visibility = "hidden";
+  //     }
+  //   });
+  // };
 
-  const observerOptions = {
-    root: null, // Use the viewport as the root
-    rootMargin: "0px", // No margin around the root
-    threshold: 0.2, // Trigger when 20% of the element is in the viewport
-  };
-  const observer = new IntersectionObserver(
-    handleIntersection,
-    observerOptions
-  );
-  observer.observe(contactForm);
+  // const observerOptions = {
+  //   root: null, // Use the viewport as the root
+  //   rootMargin: "0px", // No margin around the root
+  //   threshold: 0.2, // Trigger when 20% of the element is in the viewport
+  // };
+  // const observer = new IntersectionObserver(
+  //   handleIntersection,
+  //   observerOptions
+  // );
+  // observer.observe(contactForm);
 });
